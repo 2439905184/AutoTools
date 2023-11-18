49db92d5129b28d3160a0d46f3d2d93806a1857b
小沙盒工作室-室长 <29478722+2439905184@users.noreply.github.com>
2023-11-18 10:52:25 +0800

    Add files via upload

---
+++ 自动刷快手_auto.js
@@ -0,0 +1,13 @@
+launchApp("快手极速版")
+sleep(5600)
+while(true)
+{
+    toast("上滑")
+    var x1 = random(device.width/4,(device.width/4)*3);
+        var y1 = random((device.height/4)*3.5,(device.height/4)*3.75);
+        var x2 = random(device.width/4,(device.width/4)*3);
+        var y2 = random((device.height/4)*0.25,(device.height/4)*0.5);
+    swipe(x1, y1, x2, y2,random(500,1000));
+    var sleeptime = random(2000,5000)
+    sleep(sleeptime)
+}
